bcb7ad87d9f5402f76c4b2fa5b502a52e85a6cc3
Omor Faruk Rony <okrony356@gmail.com.com>
2022-08-16 21:40:24 +0600

    The Commit message

--- index.js
+++ index.js
@@ -2,8 +2,10 @@
   var colors = [0, 1, 2 ,3, 4, 5, 6, 7, 8, 9, "A","B","C","D","E","F"];
 
 
-
-  var col = [ 0,"A",3,3,4,6,8,7,"B",1]
+  window.addEventListener('DOMContentLoaded', (event) => {
+   document.body.style.backgroundColor="gray"
+});
+  var col = [ 0,"A",3,3,4,6,8,7,"B",1, "C","D","E","F"]
  
  const btn =document.getElementById('btn');
  const bg = document.getElementById('bg');
@@ -21,9 +23,8 @@
         
     }
 
-    bg.innerText=hex + "+" + grad;
+    bg.innerText=hex +" " + "and" + " " + grad;
     document.body.style.backgroundImage=  'linear-gradient( '+ "400deg" +',' + hex + ' , ' + grad + ')';
-    document.body.style.transition = '2s';
 
 
 
@@ -37,3 +38,5 @@
     return Math.floor(Math.random()*col.length);
     
  }
+;
+
